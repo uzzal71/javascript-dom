f8d2bf4e02bbd38138a19eb5d44058b21b8a4285
uzzal71 <uzzalroy.acm@gmail.com>
2022-11-02 00:35:50 +0600

    DOM Elements

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-document.getElementById("demo").innerHTML = "Hello DOM";
-console.dir(document.getElementById("demo"));
+// document.getElementById("demo").innerHTML = "Hello DOM";
+// console.dir(document.getElementById("demo"));
+
+const x = document.forms["frm1"];
+let text = "";
+for (i = 0; i < x.length; i++) {
+  text += x.elements[i].value + "<br/>";
+}
+
+document.getElementById("demo").innerHTML = text;
